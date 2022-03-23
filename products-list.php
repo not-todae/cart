e14a8e6a96eb89ad9943921d74216ee2c3c8196d
David Aaron Echon <echon.davidaaron@auf.edu.ph>
2022-03-23 22:33:14 +0800

    Customized the design, added my own logo, updated the layout of products, and shopping cart

--- products-list.php
+++ products-list.php
@@ -7,37 +7,76 @@ $products_data = FileUtility::openCSV('products.csv');
 
 $products = Product::convertArrayToProducts($products_data);
 
-$customer = new Customer('John Doe', 'john@mail.com');
+$customer = new Customer('David Aaron Echon', 'echon.davidaaron@auf.edu.ph');
 ?>
 <html>
 <head>
     <title>My Cart</title>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css"rel="stylesheet"/>
+    <style>
+        h1{
+        font-family: Share Tech;
+        }
+    </style>
+  </head>
 <body>
-
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Products</h2>
-<h4>
-    <a href="shopping-cart.php">Shopping Cart</a>
-</h4>
-
-<?php foreach ($products as $product): ?>
-
-<form action="add-to-cart.php" method="POST">
-    <input type="hidden" name="product_id" value="<?php echo $product->getId(); ?>" />
-    <h3><?php echo $product->getName(); ?></h3>
-    <img src="<?php echo $product->getImage(); ?>" height="100px" />
-    <p>
-        <?php echo $product->getDescription(); ?><br/>
-        <span style="color: blue">PHP <?php echo $product->getPrice(); ?></span>
-        Qty <input type="number" name="quantity" class="quantity" value="0" />
-        <button type="submit">
-            ADD TO CART
-        </button>
-    </p>
-</form>
-
-<?php endforeach; ?>
-
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <small style="font-family:Share Tech;font-size:46px">GPYOU</small>
+        </a>
+        <a class="text-reset me-3" href="shopping-cart.php">
+              <span><i class="fas fa-shopping-cart"></i></span>
+              <img src="cart-fill.svg" height="20"/>
+            </a>
+    </div>
+    </nav>
+    <div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url(https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/geforce-rtx-turing/2080-ti/gallery/geforce-rtx-2080-ti-gallery-c-641-u@2x.jpg);
+      height: 600px;
+      width: 1600px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Achieve Greater by Being You.</h1>
+          <h4 class="mb-3">Welcome! <?php echo $customer->getName(); ?> </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+    </header>
+    <?php foreach ($products as $product): ?>
+        <section style="background-color: #eee;">
+            <div class="container py-2">
+                <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    <div class="card text-black">
+                    <i class="fab fa-gpu fa-lg pt-3 pb-1 px-3"></i>
+                    <img src="<?php echo $product->getImage(); ?>"
+                        class="card-img-top"
+                    />
+                    <div class="card-body">
+                        <div class="text-center">
+                        <h3 class="card-title"><?php echo $product->getName(); ?></h3>
+                        <p class="text-muted mb-4"><?php echo $product->getDescription(); ?><br/></p>
+                    </div>
+                        <div class="d-flex justify-content-between total font-weight-bold mt-4">
+                        <span>PHP <?php echo $product->getPrice(); ?></span>
+                        <div class="d-flex flex-row">
+                    </button>
+                    <button type="button" class="btn btn-success flex-fill ms-1">Add to Cart</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+    <?php endforeach; ?>
 </body>
 </html>
--- shopping-cart.php
+++ shopping-cart.php
@@ -8,7 +8,7 @@ $products_data = FileUtility::openCSV('products.csv');
 
 $products = Product::convertArrayToProducts($products_data);
 
-$customer = new Customer('John Doe', 'john@mail.com');
+$customer = new Customer('David Aaron Echon', 'echon.davidaaron@auf.edu.ph');
 
 $shoppingCart = new ShoppingCart($customer);
 $shoppingCartItems = $shoppingCart->getAllItems();
@@ -16,14 +16,181 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 <html>
 <head>
     <title>My Cart</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" />
+    
+    <style>
+        .gradient-custom {
+       
+        background: #76B900;
+        }
+    </style>
 </head>
 <body>
+    <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- Go home-->
+        <a class="navbar-brand" href="products-list.php">
+        <small style="font-family:Share Tech;font-size:46px">GPYOU</small>
+        </a>
+        <a class="text-reset me-3" href="shopping-cart.php">
+              <span><i class="fas fa-shopping-cart"></i></span>
+              <img src="cart-fill.svg" height="20"/>
+            </a>
+    </div>
+    </nav>
+<section class="h-100 gradient-custom">
+  <div class="container py-5">
+    <div class="row d-flex justify-content-center my-4">
+      <div class="col-md-8">
+        <div class="card mb-4">
+          <div class="card-header py-3">
+            <h5 class="mb-0">Cart - 2 items</h5>
+          </div>
+          <div class="card-body">
+            <!-- Single item -->
+            <div class="row">
+              <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                <!-- Image -->
+                <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
+                  <img src="https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-630-d@2x.png"
+                    class="w-100" alt="GPU1" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
+                  </a>
+                </div>
+                <!-- Image -->
+              </div>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
-</h4>
+              <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                <!-- Data -->
+                <p><strong>RTX 3090</strong></p>
+              </div>
+
+              <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <!-- Quantity -->
+                <div class="d-flex mb-4" style="max-width: 300px">
+                  <button class="btn btn-success px-3 mb-5"
+                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                    <i class="fas fa-minus"></i>-
+                  </button>
+
+                  <div class="form-outline">
+                    <input id="form1" min="0" name="quantity" value="1" type="number" class="form-control" />
+                    <label class= "form-label" for="form1">Quantity</label>
+                  </div>
+
+                  <button class="btn btn-success px-2.9 mb-5"
+                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                    <i class="fas fa-plus"></i>+
+                  </button>
+                </div>
+                <!-- Quantity -->
+
+                <!-- Price -->
+                <p class="text-start text-md-center">
+                  <strong>₱88,000</strong>
+                </p>
+                <!-- Price -->
+              </div>
+            </div>
+            <!-- Single item -->
+
+            <hr class="my-4" />
+
+            <!-- Single item -->
+            <div class="row">
+              <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
+                <!-- Image -->
+                <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
+                  <img src="https://www.amd.com/system/files/2021-04/766267-radeon-rx-6800xt-gpus-1260x709.png"
+                    class="w-100" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
+                  </a>
+                </div>
+                <!-- Image -->
+              </div>
+
+              <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
+                <!-- Data -->
+                <p><strong>RX 6800 XT</strong></p>
+              </div>
+
+              <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <!-- Quantity -->
+                <div class="d-flex mb-4" style="max-width: 300px">
+                  <button class="btn btn-success px-3 mb-5"
+                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                    <i class="fas fa-minus"></i>-
+                  </button>
+
+                  <div class="form-outline">
+                    <input id="form1" min="0" name="quantity" value="1" type="number" class="form-control" />
+                    <label class="form-label" for="form1">Quantity</label>
+                  </div>
+
+                  <button class="btn btn-success px-2.9 mb-5"
+                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                    <i class="fas fa-plus"></i>+
+                  </button>
+                </div>
+                <!-- Quantity -->
+
+                <!-- Price -->
+                <p class="text-start text-md-center">
+                  <strong>₱31,500</strong>
+                </p>
+                <!-- Price -->
+              </div>
+            </div>
+            <!-- Single item -->
+          </div>
+        </div>
+        <div class="card mb-10">
+          <div class="card-body">
+            <p><strong>Expected shipping delivery</strong></p>
+            <p class="mb-0">3.23.2022 - 3.28.2022</p>
+          </div>
+        </div>
+        
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <div class="card-header py-3">
+            <h5 class="mb-0">Summary</h5>
+          </div>
+          <div class="card-body">
+            <ul class="list-group list-group-flush">
+              <li
+                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                Products
+                <span>₱119,500</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                Shipping
+                <span>₱500</span>
+              </li>
+              <li
+                class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                <div>
+                  <strong>Total amount</strong>
+                  <strong>
+                    <p class="mb-0">(including VAT)</p>
+                  </strong>
+                </div>
+                <span><strong>₱120,000</strong></span>
+              </li>
+            </ul>
+            <button type="button" class="btn btn-success btn-lg btn-block">
+              Go to checkout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php if (count($shoppingCartItems) > 0): ?>
 
